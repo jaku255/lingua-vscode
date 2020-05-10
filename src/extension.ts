5d48dd56a78bf6e7078b24d5bcea8f1a68cb4473
Joachim Aleszkiewicz <jaku255@gmail.com>
2020-05-10 22:45:37 +0200

    Added code to launch Language Server

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,43 @@
 import * as vscode from 'vscode';
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient';
+
+let client: LanguageClient;
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Lingua extension is now active');
 
+	let serverExecutable = vscode.workspace.getConfiguration('lingua').get<string>('serverExecutable');
+	if (serverExecutable) {
+		let serverOptions: ServerOptions = {
+			command: serverExecutable
+		};
+
+		// Options to control the language client
+		let clientOptions: LanguageClientOptions = {
+			// Register the server for plain text documents
+			documentSelector: [{ scheme: 'file', language: 'lingua' }],
+			synchronize: {
+				// Notify the server about file changes to '.clientrc files contained in the workspace
+				fileEvents: vscode.workspace.createFileSystemWatcher('**/.lingua')
+			}
+		};
+
+		// Create the language client and start the client.
+		client = new LanguageClient(
+			'linguaServer',
+			'Lingua Language Server',
+			serverOptions,
+			clientOptions
+		);
+
+		// Start the client. This will also launch the server
+		client.start();
+	}
 }
 
-export function deactivate() {}
+export function deactivate() { }
