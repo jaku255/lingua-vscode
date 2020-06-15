6c641b63894fdc2c77237c851c7a0d62170312a5
Joachim Aleszkiewicz <jaku255@gmail.com>
2020-06-15 19:08:35 +0200

    Slight improvement in syntax highlighting, added LSP debugging
    Also added a fool-proof syntax highlighting mode (lingua-primitive)

--- src/extension.ts
+++ src/extension.ts
@@ -12,18 +12,23 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Lingua extension is now active');
 
 	let serverExecutable = vscode.workspace.getConfiguration('lingua').get<string>('serverExecutable');
+	let logPath = vscode.workspace.getConfiguration('lingua').get<string>('serverLog');
+	let serverArgs : string[] = []
+	if (logPath) {
+		serverArgs = ['--log-file', logPath]
+	}
 	if (serverExecutable) {
 		let serverOptions: ServerOptions = {
-			command: serverExecutable
+			command: serverExecutable, args: serverArgs
 		};
 
 		// Options to control the language client
 		let clientOptions: LanguageClientOptions = {
 			// Register the server for plain text documents
-			documentSelector: [{ scheme: 'file', language: 'lingua' }],
+			documentSelector: [{ scheme: 'file', language: 'lingua' }, { scheme: 'file', language: 'lingua-primitive' }],
 			synchronize: {
-				// Notify the server about file changes to '.clientrc files contained in the workspace
-				fileEvents: vscode.workspace.createFileSystemWatcher('**/.lingua')
+				// We can notify the server about file changes to files in other languages contained in the workspace
+				//fileEvents: vscode.workspace.createFileSystemWatcher('**/.lingua')
 			}
 		};
 
